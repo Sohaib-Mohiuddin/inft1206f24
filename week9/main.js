3fe695557cf511634395c9281f24e8bd1691075a
Sohaib <sohaibm131@gmail.com>
2024-11-06 17:39:23 -0500

    updated

---
+++ week9/main.js
@@ -0,0 +1,9 @@
+function toUpper(string) {
+    return string.toUpperCase();
+}
+
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
